c90f1faf70a86320db783846e2139f9f3a6225d4
ShaggyWack <42456773+ShaggyWack@users.noreply.github.com>
2022-06-08 22:49:04 +0200

    - Initial Clean Project commit

---
+++ app/src/main/java/com/demo/discovery_incubator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.demo.discovery_incubator
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        super.setContentView(R.layout.activity_main)
+    }
+}
